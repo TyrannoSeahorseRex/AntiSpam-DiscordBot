e91b22b9c1c9708381c04c7413001f14fb813fe8
Sea Rex <wheeler_harry@icloud.com>
2025-01-30 20:00:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from discord.ext import tasks
 from datetime import datetime, timedelta
 
 ## CLIENT AND CUSTOM CHANGING STATUS
+
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.all())
+        self.synced = False
+        self.message_count = {}  # Track message counts
+        self.timeout_users = set()  # Track users who are timed out
